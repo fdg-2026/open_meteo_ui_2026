3172ef332c65d6af4bf92be6427915420c2cd7ee
gs <gs@xyz.de>
2026-07-18 17:37:04 +0200

    changed type in RawAutocomplete from String to LocationData and show AutoComplete options in a separated ListView

--- lib/location/location_page.dart
+++ lib/location/location_page.dart
@@ -12,18 +12,20 @@ class LocationPage extends StatefulWidget {
 }
 
 class _LocationPageState extends State<LocationPage> {
-  final List<String> _options = const [
-    "Berlin",
-    "London",
-    "Madrid",
-    "Rome",
-    "Test01",
-    "Test02",
-    "Test03",
-    "Test04",
-    "Test05",
-    "Test06",
-    "Test07",
+  final List<LocationData> _testLocations = [
+    LocationData(name: "Berlin", latitude: 10, longitude: 11),
+    LocationData(name: "London", latitude: 20, longitude: 22),
+    LocationData(name: "Madrid", latitude: 30, longitude: 33),
+    LocationData(name: "Rome", latitude: 40, longitude: 44),
+    LocationData(name: "Test01", latitude: 89, longitude: 1),
+    LocationData(name: "Test02", latitude: 89, longitude: 2),
+    LocationData(name: "Test03", latitude: 89, longitude: 3),
+    LocationData(name: "Test04", latitude: 89, longitude: 4),
+    LocationData(name: "Test05", latitude: 89, longitude: 5),
+    LocationData(name: "Test06", latitude: 89, longitude: 6),
+    LocationData(name: "Test07", latitude: 89, longitude: 7),
+    LocationData(name: "Test08", latitude: 89, longitude: 8),
+    LocationData(name: "Test09", latitude: 89, longitude: 9),
   ];
 
   @override
@@ -43,29 +45,32 @@ class _LocationPageState extends State<LocationPage> {
             const SizedBox(height: 20),
             Text("Add a new location:", style: TextStyle(fontSize: 20)),
             const SizedBox(height: 10),
-            RawAutocomplete<String>(
+            RawAutocomplete<LocationData>(
               textEditingController: TextEditingController(),
               focusNode: FocusNode(),
 
               // 1️⃣ Filter logic
               optionsBuilder: (TextEditingValue textEditingValue) {
                 if (textEditingValue.text.isEmpty) {
-                  return const Iterable<String>.empty();
+                  return const Iterable<LocationData>.empty();
                 }
-                return _options.where(
-                  (option) => option.toLowerCase().contains(
+                return _testLocations.where(
+                  (location) => location.name.toLowerCase().contains(
                     textEditingValue.text.toLowerCase(),
                   ),
                 );
               },
 
               // 2️⃣ What happens when user selects an option
-              onSelected: (String selection) {
-                widget.locationProvider.addLocation(
-                  LocationData(name: selection, latitude: 89, longitude: 0),
-                );
+              onSelected: (LocationData selection) {
+                widget.locationProvider.addLocation(selection);
                 Navigator.pop(context);
               },
+
+              // Next line is needed when you use your own type in RawAutocomplete instead of String.
+              // It is used to display the selected item in the TextField.
+              // Because we directly switch back to ForecastPage after an item was selected, it's not really needed in our case.
+              displayStringForOption: (LocationData location) => location.name,
 
               // 3️⃣ The TextField itself
               fieldViewBuilder:
@@ -92,19 +97,23 @@ class _LocationPageState extends State<LocationPage> {
                   borderRadius: BorderRadius.circular(8),
                   child: ConstrainedBox(
                     constraints: const BoxConstraints(maxHeight: 200),
-                    child: ListView.builder(
+                    child: ListView.separated(
                       //padding: EdgeInsets.zero,
                       //shrinkWrap: true,
                       itemCount: options.length,
                       itemBuilder: (context, index) {
                         final option = options.elementAt(index);
-                        return getAutocompleteTile(
-                          options.elementAt(index),
-                          () {
-                            onSelected(option);
-                          },
-                        );
+                        return getAutocompleteTile(option, () {
+                          onSelected(option);
+                        });
                       },
+                      // Gemini answering on _ and __ in netxt line:
+                      // When you see (_, __), it's a way for a developer to tell the compiler
+                      // — and other developers— that the function requires certain arguments to be passed in,
+                      // but the code inside the function doesn't actually need to use them.
+                      // You use two different symbols (_ and __) because Dart doesn't allow you
+                      // to have two variables with the exact same name in the same scope.
+                      separatorBuilder: (_, __) => const Divider(height: 1),
                     ),
                   ),
                 );
@@ -116,14 +125,27 @@ class _LocationPageState extends State<LocationPage> {
     );
   }
 
-  Widget getAutocompleteTile(String text, VoidCallback callbackOnTap) {
+  Widget getAutocompleteTile(
+    LocationData location,
+    VoidCallback callbackOnTap,
+  ) {
     // InkWell (dt.: Tintenfass) - it "spreads out" when tapped
     return InkWell(
       onTap: () {
         callbackOnTap();
       },
       child: ListTile(
-        title: Text(text, style: const TextStyle(fontWeight: FontWeight.bold)),
+        title: Text(
+          location.name,
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
+        trailing: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("lat:${location.latitude.toStringAsFixed(3)}"),
+            Text("lon:${location.longitude.toStringAsFixed(3)}"),
+          ],
+        ),
       ),
     );
   }
